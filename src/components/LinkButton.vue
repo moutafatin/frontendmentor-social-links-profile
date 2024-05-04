575c16f0bf13d444c9b356ddac4bad1e796962d0
Oussama Moutafatin <moutafatin3@gmail.com>
2024-05-04 12:06:13 +0100

    little refactor

---
+++ src/components/LinkButton.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+
+defineProps<{
+  link : {
+    name:string,
+    url:string
+  }
+}>()
+</script>
+
+
+
+<template>
+  <a :href="link.url" class="w-full text-white font-semibold bg-neutral-gray py-3 transition-colors hover:bg-primary hover:text-neutral-dark-gray inline-flex justify-center rounded-lg">{{link.name}}</a>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,19 +1,19 @@
-
 <script setup lang="ts">
-import ProfileImage  from '@/assets/images/avatar-jessica.jpeg';
+import ProfileImage from '@/assets/images/avatar-jessica.jpeg'
+import LinkButton from '@/components/LinkButton.vue'
 
 const profile = {
-  avatarUrl : ProfileImage,
-  fullName : 'Jessica Randall',
-  country : 'United Kingdom' ,
-city :' London',
-  summary : 'Front-end developer and avid reader.',
-  links : [
-    {name : 'GitHub', url : '#'},
-    {name : 'Frontend Mentor', url : '#'},
-    {name : 'Linkedin', url : '#'},
-    {name : 'Twitter', url : '#'},
-    {name : 'Instagram', url : '#'},
+  avatarUrl: ProfileImage,
+  fullName: 'Jessica Randall',
+  country: 'United Kingdom',
+  city: ' London',
+  summary: 'Front-end developer and avid reader.',
+  links: [
+    { name: 'GitHub', url: '#' },
+    { name: 'Frontend Mentor', url: '#' },
+    { name: 'Linkedin', url: '#' },
+    { name: 'Twitter', url: '#' },
+    { name: 'Instagram', url: '#' }
   ]
 
 }
@@ -21,17 +21,18 @@ city :' London',
 
 <template>
   <main class="p-5 flex items-center justify-center min-h-screen">
-    <div class="bg-neutral-dark-gray rounded-2xl p-6 text-center max-w-xs mx-auto space-y-6 md:p-8 md:max-w-[22rem] w-full">
-     <div class="space-y-2">
-       <img :src="profile.avatarUrl" alt="Profile image" class="size-20 rounded-full mx-auto md:size-24">
-       <h1 class="text-white font-bold text-2xl md:text-3xl">{{profile.fullName}}</h1>
-       <h2 class="text-primary font-semibold">{{profile.city}}, {{profile.country}}</h2>
-     </div>
-      <p class="text-white text-sm md:text-base">"{{profile.summary}}"</p>
+    <div
+      class="bg-neutral-dark-gray rounded-2xl p-6 text-center max-w-xs mx-auto space-y-6 md:p-8 md:max-w-[22rem] w-full">
+      <div class="space-y-2">
+        <img :src="profile.avatarUrl" alt="Profile image" class="size-20 rounded-full mx-auto md:size-24">
+        <h1 class="text-white font-bold text-2xl md:text-3xl">{{ profile.fullName }}</h1>
+        <h2 class="text-primary font-semibold">{{ profile.city }}, {{ profile.country }}</h2>
+      </div>
+      <p class="text-white text-sm md:text-base">"{{ profile.summary }}"</p>
 
       <ul class="space-y-4">
         <li v-for="(link,index) in profile.links" :key="index">
-          <a :href="link.url" class="w-full text-white font-semibold bg-neutral-gray py-3 transition-colors hover:bg-primary hover:text-neutral-dark-gray inline-flex justify-center rounded-lg">{{link.name}}</a>
+          <LinkButton :link="link" />
         </li>
       </ul>
     </div>
